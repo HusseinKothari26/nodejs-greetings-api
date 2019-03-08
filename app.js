d73e6561e5470b66b29753cd765c2f15a0639f66
hussein.kothari@cgi.com <hussein.kothari@cgi.com>
2019-03-08 12:10:58 -0500

    Added app.js with hello api

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Api Endpoint to display greeting, taking name as a parameter.
+app.get('/hello/:name?', function(req, res, next) {
+    var name = req.params.name;
+    // Check if parameter missing, greet a stranger.
+    if (!name) {
+        // Response greeting with name.
+        res.send('Hello Stranger!.');
+        return;
+    }
+    else {
+        // If name parameter present save in datastore and greet.
+        const fs = require('fs')
+        // Check if file already existed.
+        fs.access('./Greeted_Users_Db.json', fs.F_OK, (err) => {
+        if (err) {
+            // File does not exists so create file.
+            var data = [{"name": name }]
+            var writeData = fs.writeFile("Greeted_Users_Db.json", JSON.stringify(data,null,2), (err, result) => { // Write File
+                if (err) {
+                    return console.error(err);
+                } else {
+                    console.log("Successfully created file and saved name");
+                }
+            });
+        }
+        // File Exists
+        fs.readFile("Greeted_Users_Db.json", (err, data) => { // Read File
+            if (err) {
+                return console.error(err);
+            };
+            var data = JSON.parse(data);
+            data.push({"name": name})
+            var writeData = fs.writeFile("Greeted_Users_Db.json", JSON.stringify(data,null,2), (err, result) => { // Write File
+                if (err) {
+                    return console.error(err);
+                } else {
+                    console.log("Successfully saved name");
+                }
+            });
+        });
+    });
+    // Response greeting with name.
+    res.send('Hello ' + name + '!.');
+    }
+});
+app.listen(port, () => console.log(`Greetings app is listening on port ${port}!`))
